67a4ae1d4fb49a68b833440fee0276d74799b681
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2026-07-31 02:26:48 -0300

    Refactor gruposwats.js to use ES6 imports

--- gruposwats.js
+++ gruposwats.js
@@ -1,5 +1,5 @@
-const puppeteer = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
 puppeteer.use(StealthPlugin());
 
