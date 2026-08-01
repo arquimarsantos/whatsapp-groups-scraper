53aa35e12565b5704992ad8ad2b94c718c114e6f
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2026-08-01 04:16:10 -0300

    Add files via upload

---
+++ cron.js
@@ -0,0 +1,29 @@
+import cron from 'node-cron';
+import { runScraper } from './gruposwats.js';
+
+let running = false;
+
+async function executeScraper() {
+    if (running) {
+        //console.log('Scraper já está executando');
+        return;
+    }
+
+    running = true;
+
+    try {
+        console.log('Iniciando scraper...');
+        await runScraper();
+        console.log('Scraper finalizado');
+    } catch (e) {
+        console.error(e.message);
+    } finally {
+        running = false;
+    }
+}
+
+executeScraper();
+
+cron.schedule('*/30 * * * *', executeScraper);
+
+console.log('Cron iniciado');
