961069b06902caa3189e9be747292182e543c87a
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2026-08-01 05:22:42 -0300

    Refactor and optimize group scraping logic

--- gruposwats.js
+++ gruposwats.js
@@ -2,6 +2,7 @@ import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
 puppeteer.use(StealthPlugin());
+
 /*
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
